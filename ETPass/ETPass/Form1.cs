1898762ff0f63f530b868d6612dd8bd4c42b51a3
Unqown Exe <76444282+unqownexe@users.noreply.github.com>
2021-10-18 18:11:01 +0300

    Add files via upload

---
+++ ETPass/ETPass/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+namespace ETPass
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Show();
+            Clipboard.SetText("H3gY!*pA79fK5%1");
+            //MessageBox.Show("Şifre başarıyla panoya kopyalandı.", "EBAAdmin");
+            this.Hide();
+            Application.Exit();
+        }
+    }
+}
